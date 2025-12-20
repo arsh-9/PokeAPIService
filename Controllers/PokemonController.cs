671cc22ccf6de641062b77c368b0823f76ea5c2e
Arsh <coco@Arshs-MacBook-Air.local>
2025-12-20 14:35:42 +0530

    Initial commit

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PokeAPIService.Controllers;
+
+[ApiController]
+[Route("api/pokemon")]
+public class PokemonController : ControllerBase
+{
+    public PokemonController()
+    {
+
+    }
+}
+
